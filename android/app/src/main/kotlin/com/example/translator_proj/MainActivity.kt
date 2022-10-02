5588fa613e0ed84acc9bda3492ad42825a08367c
Temitope Ologunbaba <temitope@glade.ng>
2022-10-02 09:00:47 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/translator_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.translator_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/lib/src/extension.dart
@@ -0,0 +1,7 @@
+import '../translator.dart';
+
+extension StringExtension on String {
+  Future<Translation> translate(
+          {String from = 'auto', String to = 'en'}) async =>
+      await GoogleTranslator().translate(this, from: from, to: to);
+}
